7c3547886727706d1eb8689560c57712abfb548f
Jonas U <jonasunsinn@gmail.com>
2016-08-24 23:20:19 +0200

    made code less stupid

--- example.py
+++ example.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
 Basic usage example of the nesctrl module
 """
 import nesctrl
 nesctrl.setup(22, 17, 4)
 
-while 1:
-    try:
+try:
+    while 1:
         if nesctrl.read_controller_state()["A"]:
-            print("A button pressed!")
+            print("'A' button pressed.")
         else:
-            print("A button not pressed")
-    except KeyboardInterrupt:
+            print("'A' button not pressed.")
+
+except KeyboardInterrupt:
         nesctrl.cleanup()
-        break
